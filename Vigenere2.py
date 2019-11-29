147107ce1c28125d8076d5a3bb916f8dc4c77a77
aclinch <NvnJgDvyvtckB4Fhmj6QCCZlkZb2kd>
2019-11-29 17:58:57 +0000

    Done 3

--- Vigenere2.py
+++ Vigenere2.py
@@ -1,15 +1,36 @@
 from collections import Counter
 
+import CipherUtils
+
 ENCRYPTED = "PTMDWEPZZLFVSIBYSQMMCYFXABGKIAEMQDLXOLPTJQPLGYXLLNPLQBZNKTRAHPGNLXNKCAXBOXYDWFGMJJXEPKMYWFOMPLXFZVRSEQTNQSIPSWSWHELDCESYFZLQSOHPYEWEPPYOHLYMZFVKDPCXYPEILOXEPZCHEPLVCYHLQQRWMHPIJWZFWTYRIDTZJDWEPEYDABWTECSRYLCOMKEPCSSIJAACMMECPPWXYLFLHAFBGQYIWGQEYATMBELBSQQESOTMQZJXSWJLLXYLYSSITJYSEKOSLPAQSMGYJBCMLNIPEWZPHOLELELBCMDCSJMCRHLBYBFPWXXMOFIPEQMYEOZACHMQSZCREOOBMELBMIZJMQSIBLZBCGBTJCPZCYXZZTMFVEPZBLVITVEHEPLJMFXQZLGPEKOVMDEIGIRTSKTBULWKPIPWCYPLRTQBMCRDLBCCQSIAOWUYWQLQPDEKOIQVIATNQSIJTOFEWBYLDZVQSMNLVPZVRSIJZUCYXELXNPRBOBMMILYMYEAETKFSIOQIRSIODACYWBZNRSIXYBGBYBYWZTPFEGMQLFDNYXMIJEYDLFRPCDXXYLFTWPPVQTXFGMLPWPEWRSIPXCBRITSQASXBDAFLHPPBSASKEPYERLMQJTXVXWQETOZVMFRZPLDZVEPPYONRDBPPXRCVCOJOZUFTWTPMIWCYZWXPEQCWJWMSPZQTRKYWNLVPZVQSSRWLAZQBTVQTHBSQQOSLCPCOIZWIPPHMCGGYKFYBMSMPLNDLMODRSDXQSMLHLBYJWSIODPYXIFEPYOFBNWKPQLCMLPGBDAYCCQSILPZBCBMSMAPBFPQE"
 KEY_LENGTH = 6
 ENCRYPTED_LENGTH = len(ENCRYPTED)
 TESS26 = open("data/tess26.txt", "r").readline()
 
-same_letter_segments = [ENCRYPTED[i::6] for i in range(0, 6)]
+sameLetterSegments = [ENCRYPTED[i::6] for i in range(0, 6)]
+tess26LetterFrequencies = [kvp[0] for kvp in Counter(TESS26).most_common()]
 
-tess26Counter = Counter(TESS26)
-segmentCounters = [Counter(segment) for segment in same_letter_segments]
+counters = [Counter(segment) for segment in sameLetterSegments]
+mostCommonLettersInEachSegment = [counter.most_common()[0][0] for counter in counters]
 
-print([ord(kvp[0]) for kvp in tess26Counter.most_common()])
-print([ord(kvp[0]) for kvp in segmentCounters[0].most_common()])
+# Currently a hack so that I can finish the rest of the exercises
+mostCommonLettersInEachSegment[3] = "P"
 
+print(mostCommonLettersInEachSegment)
+
+steps = [ord("E") - ord(commonLetter) for commonLetter in mostCommonLettersInEachSegment]
+
+print(steps)
+
+decrypted = [CipherUtils.incrementer(sameLetterSegments[x], steps[x]) for x in range(len(steps))]
+
+finalString = ""
+for i in range(len(decrypted[0])):
+    for j in range(len(decrypted)):
+        finalString += decrypted[j][i]
+print(finalString)
+
+if finalString in TESS26:
+    print("Success")
+else:
+    print("Failure")
