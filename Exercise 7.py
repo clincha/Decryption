b6f5b85d36993c65e6e93a27e5e43e9239ab902b
aclinch <NvnJgDvyvtckB4Fhmj6QCCZlkZb2kd>
2019-12-03 12:21:10 +0000

    Done 7

--- Exercise 7.py
+++ Exercise 7.py
@@ -11,25 +11,67 @@ tessMostCommon = Counter(TESS27).most_common()
 tessMostCommonWords = Counter(TESS27.split("|")).most_common()
 encryptedMostCommon = Counter(ENCRYPTED).most_common()
 
-print(tessMostCommon)
-print(encryptedMostCommon)
+# print(tessMostCommon)
+# print(encryptedMostCommon)
 
 replacements = {}
 for index in range(len(ALPHABET)):
     replacements[encryptedMostCommon[index][0]] = tessMostCommon[index][0]
 
+
+replacements["B"] = "A"
+replacements["Y"] = "T"
+
 replacements["D"] = "T"
-replacements["B"] = "O"
+replacements["Y"] = "O"
 
 replacements["Y"] = "H"
-replacements["S"] = "A"
+replacements["K"] = "O"
+
+replacements["K"] = "N"
+replacements["Z"] = "O"
+
+replacements["J"] = "O"
+replacements["Z"] = "S"
+
+replacements["G"] = "F"
+replacements["V"] = "P"
+
+replacements["O"] = "R"
+replacements["Q"] = "I"
+
+replacements["X"] = "G"
+replacements["H"] = "W"
+
+replacements["T"] = "W"
+replacements["H"] = "M"
+
+replacements["S"] = "B"
+replacements["F"] = "Y"
+
+replacements["V"] = "U"
+replacements["M"] = "P"
+
+replacements["H"] = "K"
+replacements["A"] = "M"
+
+replacements["F"] = "P"
+replacements["M"] = "Y"
+
+replacements["A"] = "V"
+replacements["C"] = "M"
+
+replacements["M"] = "M"
+replacements["C"] = "Y"
+
+replacements["I"] = "Z"
+replacements["U"] = "X"
 
 print(replacements)
 
 decrypted = CipherUtils.replace(ENCRYPTED, replacements)
 
 words = decrypted.split("|")
-words = [word if len(word) < 4 else None for word in words]
 print(tessMostCommonWords)
 print(Counter(words).most_common())
 
